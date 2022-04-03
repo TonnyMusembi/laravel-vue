371e8bdd336fbdc2589b527af7acd2f319aef6d4
Tonny <musembitonnyj@gmail.com>
2022-04-03 23:47:38 +0300

    add

---
+++ resources/js/App.vue
@@ -0,0 +1,28 @@
+<!-- component -->
+<template>
+    <!-- component -->
+<button @click="next">Next</button>
+
+	<button @click="greet">Greet</button>
+
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: 'Vue.js'
+    }
+  },
+  methods: {
+    greet(event) {
+      // `this` inside methods points to the current active instance
+      alert(`Hello ${this.name}!`)
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
+  }
+}
+
+</script>
